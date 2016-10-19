4e58dd795f49d049629c126e73db74f25efe467f
Aleksandr Kuznetsov <aku.ru.kz@gmail.com>
2016-10-19 14:27:33 +0300

    Fix for docker image build - backported python packages versions

--- build.sh
+++ build.sh
@@ -7,6 +7,17 @@ apk -U add --virtual temp python-dev py-pip alpine-sdk python py-setuptools
 
 # Install python dependencies
 pip install --upgrade pip
+
+pip install urllib3==1.17
+pip install ConcurrentLogHandler==0.9.1
+pip install docker-py==1.7.2
+pip install docopt==0.6.2
+pip install netaddr==0.7.18
+pip install prettytable==0.7.2
+pip install PyInstaller==3.1.1
+pip install PyYAML==3.11
+pip install requests==2.9.1
+
 pip install -r https://raw.githubusercontent.com/projectcalico/libcalico/v0.14.0/build-requirements.txt
 pip install git+https://github.com/projectcalico/libcalico.git@v0.14.0
 pip install simplejson 
